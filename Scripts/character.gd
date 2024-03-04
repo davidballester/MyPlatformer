b791fc5c045a8fb84354d64b275105dc9609b145
David Ballester Mena <david.ballester.mena@gmail.com>
2024-03-04 18:52:00 +0100

    Position character and camera in level

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -8,3 +8,8 @@ func _ready():
 	animated_sprite2d.sprite_frames = sprite_frames
 	animated_sprite2d.animation = "idle"
 	animated_sprite2d.play()
+
+func get_height():
+	var height = sprite_frames.get_frame_texture("idle", 0).get_height()
+	print("Character.get_height ", height)
+	return height
--- Scripts/game.gd
+++ Scripts/game.gd
@@ -17,14 +17,28 @@ func load_level(level: int, character_sprite_frames: SpriteFrames):
 	var level_path = "res://scenes/levels/Level" + str(level) + ".tscn"
 	print("Game.load_level ", level_path)
 	var level_scene = load(level_path).instantiate()
-	var character = create_character(character_sprite_frames)
-	level_scene.add_child(character)
+	set_up_character(character_sprite_frames, level_scene)
 	var existing_level: Level = level_container.get_node_or_null("Level")
 	if existing_level:
 		existing_level.queue_free()
 	level_container.add_child(level_scene)
 	
-func create_character(character_sprite_frames: SpriteFrames):
+func set_up_character(character_sprite_frames: SpriteFrames, level: Level):
 	var character: Character = load("res://scenes/Character.tscn").instantiate()
 	character.sprite_frames = character_sprite_frames
+	level.add_child(character)
+	character.ready.connect(position_character.bind(character, level))
+	character.ready.connect(add_camera.bind(character))
 	return character
+
+func position_character(character: Character, level: Level):
+	var level_background: TextureRect = level.get_node("Background")
+	var y = level_background.offset_bottom - character.get_height() - 100
+	var x = 100
+	print("Game.position_character x=", x, " y=", y)
+	character.position.x = x
+	character.position.y = y
+	
+func add_camera(character: Character):
+	var camera: Camera2D = load("res://scenes/level_components/Camera.tscn").instantiate()
+	character.add_child(camera)
