09a3e991cce1520109de1629aa0d6d6deda4672d
David Ballester Mena <david.ballester.mena@gmail.com>
2024-03-06 21:40:19 +0100

    Simplify character setup

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -26,32 +26,29 @@ func load_level(level: int, character_sprite_frames: SpriteFrames):
 	manage_collectibles(level_scene)
 		
 func set_up_character(character_sprite_frames: SpriteFrames, level: Level):
-	var character: Character = load("res://scenes/character/Character.tscn").instantiate()
+	var character: Character = load("res://scenes/character/PlayableCharacter.tscn").instantiate()
 	character.sprite_frames = character_sprite_frames
-	var movement_input_component: CharacterMovementInputComponent = load("res://scenes/character/CharacterMovementInputComponent.tscn").instantiate()
-	movement_input_component.character = character
-	character.add_child(movement_input_component)
 	level.character = character
 	character.ready.connect(position_character.bind(character, level))
-	character.ready.connect(add_camera.bind(character, level))
+	character.ready.connect(set_up_camera.bind(character, level))
 	return character
 
-func position_character(character: Character, level: Level):
+func position_character(character: PlayableCharacter, level: Level):
 	var level_background: TextureRect = level.get_node("Background")
 	var y = level_background.offset_bottom - character.get_height() - 100
 	var x = 100
 	print("Game.position_character x=", x, " y=", y)
 	character.position.x = x
 	character.position.y = y
+	character.camera.reset_smoothing()
 	
-func add_camera(character: Character, level: Level):
-	var camera: Camera2D = load("res://scenes/levels/components/Camera.tscn").instantiate()
+func set_up_camera(character: PlayableCharacter, level: Level):
+	var camera: Camera2D = character.camera
 	camera.limit_left = 0
 	camera.limit_top = 0
 	var level_background: TextureRect = level.get_node("Background")
 	camera.limit_right = floori(level_background.offset_right)
 	camera.limit_bottom = floori(level_background.offset_bottom)
-	character.add_child(camera)
 
 func manage_collectibles(level: Level):
 	for child in level.get_node("Collectibles").get_children():
