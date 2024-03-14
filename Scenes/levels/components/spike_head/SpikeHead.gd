f7cdfb76e63c92a482926803cf654186cb29a720
David Ballester Mena <david.ballester.mena@gmail.com>
2024-03-14 21:31:49 +0100

    Change Spike Head base type

--- Scenes/levels/components/spike_head/SpikeHead.gd
+++ Scenes/levels/components/spike_head/SpikeHead.gd
@@ -1,21 +1,17 @@
-extends Path2D
+extends Area2D
 class_name SpikeHead
 
 const BLINK_PERIOD_S = 3
 
 @export var speed: float = 50.0
 @onready var timer = $Timer
-@onready var animated_sprite = $SpikeHead/AnimatedSprite2D
-@onready var path_follow: PathFollow2D = $PathFollow2D
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _ready() -> void:
 	animated_sprite.animation = "idle"
 	animated_sprite.play()
 	timer.start(BLINK_PERIOD_S)
 	timer.timeout.connect(blink)
-
-func _process(delta):
-	path_follow.progress += delta * speed
 
 func blink() -> void:
 	animated_sprite.animation = "blink"
@@ -25,7 +21,7 @@ func idle() -> void:
 	animated_sprite.animation_looped.disconnect(idle)
 	animated_sprite.animation = "idle"
 
-func _on_spike_head_body_entered(body):
+func _on_body_entered(body):
 	if not body is Character:
 		return
 	var character: Character = body
